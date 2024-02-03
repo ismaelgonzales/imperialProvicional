0c03fb65b1db176038775bf075005887041d0674
Luis Hernandez <luis.hernandez.albia@gmail.com>
2024-02-02 22:38:54 -0500

    Mejora: registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import {
     MatFormFieldModule,
 } from '@angular/material/form-field';
 import { RegisterComponent } from './auth/register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
@@ -45,6 +46,8 @@ import { RegisterComponent } from './auth/register/register.component';
         NgbModule,
         MatFormFieldModule,
         MatInputModule,
+        ReactiveFormsModule,
+        FormsModule,
     ],
     providers: [
         provideAnimationsAsync(),
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-register',
     templateUrl: './register.component.html',
     styleUrl: './register.component.scss',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+    public loginForm!: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.initialForm();
+    }
+
+    private initialForm(): void {
+        this.loginForm = this.fb.group({
+            nombre: ['', [Validators.required]],
+            apePaterno: ['', Validators.required],
+            apeMaterno: ['', Validators.required],
+            telefono: [
+                '',
+                [
+                    Validators.required,
+                    Validators.minLength(9),
+                    Validators.maxLength(9),
+                ],
+            ],
+            correo: ['', Validators.required],
+            contrasena: ['', [Validators.required, Validators.minLength(5)]],
+        });
+    }
+}
