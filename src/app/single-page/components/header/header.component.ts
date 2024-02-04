25c39275e47342cc8d6695af8a4dde3f8a9610ae
Luis Hernandez <luis.hernandez.albia@gmail.com>
2024-02-04 03:39:58 -0500

    Mejora: header fixed

--- src/app/single-page/components/header/header.component.ts
+++ src/app/single-page/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import * as constantShared from '../../../shared/constants';
 import { Router } from '@angular/router';
 
@@ -9,6 +9,13 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent {
     public constantShared: typeof constantShared = constantShared;
+
+    shouldApplyFixedHeader = false;
+
+    @HostListener('window:scroll', ['$event'])
+    onScroll() {
+        this.shouldApplyFixedHeader = window.scrollY > 100;
+    }
 
     constructor(private _router: Router) {}
 
